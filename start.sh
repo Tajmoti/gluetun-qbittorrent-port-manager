a34a76724cb7a31520a71c63b755b0c801139c17
Timotej Šulík <timotej.sulik@gmail.com>
2025-06-08 00:18:20 +0200

    Added API call error handling

--- start.sh
+++ start.sh
@@ -3,13 +3,20 @@
 COOKIES="/tmp/cookies.txt"
 API_PATH="${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2"
 
-update_port () {
+try_update_port () {
   PORT=$(cat $PORT_FORWARDED)
   rm -f $COOKIES
-  curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "$API_PATH/auth/login" > /dev/null
-  curl -s -b $COOKIES --data 'json={"listen_port": "'"$PORT"'"}' "$API_PATH/app/setPreferences" > /dev/null
+  curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "$API_PATH/auth/login" > /dev/null || { echo "Failed to log in to qbittorrent"; return 1; }
+  curl -s -b $COOKIES --data 'json={"listen_port": "'"$PORT"'"}' "$API_PATH/app/setPreferences" > /dev/null || { echo "Failed to update qbittorrent port"; return 1; }
   rm -f $COOKIES
   echo "Successfully updated qbittorrent to port $PORT"
+}
+
+update_port() {
+  until try_update_port; do
+    echo "Trying again in 10 seconds"
+    sleep 10
+  done
 }
 
 while true; do
