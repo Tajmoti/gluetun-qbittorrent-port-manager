d48c61f82a3b208fddd9e1f47b581b466e0ec468
Timotej Šulík <timotej.sulik@gmail.com>
2025-10-07 23:29:39 +0200

    Quoted everything

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ COOKIES="/tmp/cookies.txt"
 API_PATH="${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2"
 
 try_update_port () {
-  PORT=$(cat $PORT_FORWARDED)
+  PORT=$(cat "$PORT_FORWARDED")
   rm -f $COOKIES
   curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "$API_PATH/auth/login" > /dev/null || { echo "Failed to log in to qbittorrent"; return 1; }
   curl -s -b $COOKIES --data 'json={"listen_port": "'"$PORT"'"}' "$API_PATH/app/setPreferences" > /dev/null || { echo "Failed to update qbittorrent port"; return 1; }
@@ -20,9 +20,9 @@ update_port() {
 }
 
 while true; do
-  if [ -f $PORT_FORWARDED ]; then
+  if [ -f "$PORT_FORWARDED" ]; then
     update_port
-    inotifywait -mq -e close_write $PORT_FORWARDED | while read change; do
+    inotifywait -mq -e close_write "$PORT_FORWARDED" | while read change; do
       update_port
     done
   else
