e63613925bccce3629d2743046c14ccd62fc9bd4
Timotej Šulík <timotej.sulik@gmail.com>
2025-10-08 00:20:49 +0200

    Added file deletion and recreating handling

--- start.sh
+++ start.sh
@@ -4,30 +4,23 @@ COOKIES="/tmp/cookies.txt"
 API_PATH="${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2"
 
 try_update_port () {
-  PORT=$(cat "$PORT_FORWARDED")
+  port=$(cat "$PORT_FORWARDED")
+  [ "$port" ] || { echo "Unable to read a port from $PORT_FORWARDED"; return 1; }
   rm -f $COOKIES
-  curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "$API_PATH/auth/login" > /dev/null || { echo "Failed to log in to qbittorrent"; return 1; }
-  curl -s -b $COOKIES --data 'json={"listen_port": "'"$PORT"'"}' "$API_PATH/app/setPreferences" > /dev/null || { echo "Failed to update qbittorrent port"; return 1; }
+  curl -s -c $COOKIES --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "$API_PATH/auth/login" > /dev/null || { echo "Failed to log in to qbittorrent"; return 2; }
+  curl -s -b $COOKIES --data 'json={"listen_port": "'"$port"'"}' "$API_PATH/app/setPreferences" > /dev/null || { echo "Failed to update qbittorrent port"; return 2; }
   rm -f $COOKIES
-  echo "Successfully updated qbittorrent to port $PORT"
+  echo "Successfully updated qbittorrent to port $port"
 }
 
-update_port() {
-  until try_update_port; do
+dir="$(dirname "$PORT_FORWARDED")"
+nam="$(basename "$PORT_FORWARDED")"
+
+while read -r _; do
+  while true; do
+    try_update_port
+    [ $? -ne 2 ] && break
     echo "Trying again in 10 seconds"
     sleep 10
   done
-}
-
-while true; do
-  if [ -f "$PORT_FORWARDED" ]; then
-    update_port
-    inotifywait -mq -e close_write "$PORT_FORWARDED" | while read change; do
-      update_port
-    done
-  else
-    echo "Couldn't find file $PORT_FORWARDED"
-    echo "Trying again in 10 seconds"
-    sleep 10
-  fi
-done
+done < <(echo "init"; inotifywait -mq --format '%e' -e close_write -e delete "$dir" --include "$nam")
